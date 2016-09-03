e790956b6517e5fc22fe00335a62edf7b6e086da
Arrvindh Shriraman <ashriram@ashriram-macbook-2.local>
2016-09-03 13:32:05 -0700

    Updated website

--- _site/public/js/images.js
+++
@@ -1,7 +0,0 @@
-var image = document.createElement("img");
-var imageParent = document.getElementById("body");
-image.id = "id";
-image.className = "class";
-image.src = "/Users/ashriram/Documents/Website/website/ashriram.github.io/_site/anooppicture.jpeg";         
-// image.src = "IMAGE URL/PATH"
-imageParent.appendChild(image);
--- _site/sfu_push.sh
+++
@@ -1,2 +0,0 @@
-jekyll build --config=_config.yml,_sfuconfig.yml
-rsync --delete -avz _site/ anoop@linux.cs.sfu.ca:/fas-info/cs/people/Faculty/anoop
